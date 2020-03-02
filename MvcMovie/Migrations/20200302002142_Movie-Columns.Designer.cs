3227f3aeb79f47fcdab711c637a45665a1921a18
Miyo <Miyo@DESKTOP-VOHF5HE>
2020-03-01 18:22:13 -0600

    Movie Model Columns

--- MvcMovie/Migrations/20200302002142_Movie-Columns.Designer.cs
+++ MvcMovie/Migrations/20200302002142_Movie-Columns.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcMovie.Data;
 
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200302002142_Movie-Columns")]
+    partial class MovieColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -7,15 +7,21 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3, ErrorMessage ="Debes escribir un título entre 3 y 60 caracteres"), Required(ErrorMessage = "Debes escribir un título entre 3 y 60 caracteres")]
         public string Title { get; set; }
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+        [Display(Name = "Release Date"), DataType(DataType.Date, ErrorMessage ="Debes elegir una fecha"), DisplayFormat(DataFormatString ="{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), Required(ErrorMessage ="Debes escribir un género"), StringLength(30)]
         public string Genre { get; set; }
 
+        [Range(1, 100), DataType(DataType.Currency), Required(ErrorMessage ="Debes especificar un precio")]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
         public string Rating { get; set; }
     }
 }
