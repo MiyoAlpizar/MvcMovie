6f0c2a2123b313f4fc0a60e905c49fe0fe5dcd81
Miyo <Miyo@DESKTOP-VOHF5HE>
2020-03-01 18:01:29 -0600

    Add project files.

---
+++ MvcMovie/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
